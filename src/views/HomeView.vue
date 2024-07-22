0b6d5ae5933f6475cc5c12f6fa56390e5f15eb1d
YouLikeADream <127381583+YouLikeADream@users.noreply.github.com>
2024-07-22 14:29:36 +0700

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,8 +25,7 @@
         </svg>Download Resume</button> -->
     </div>
     <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="public/download.jpeg
-      ">
+        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://telegra.ph/file/43d9cfc9f2fecf34c0b59.jpg">
     </div>
   </main>
 </template>
