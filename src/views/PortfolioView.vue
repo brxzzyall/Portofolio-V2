ae21c87bf39399f3bc2f799debd46df2c9b27dab
YouLikeADream <127381583+YouLikeADream@users.noreply.github.com>
2024-07-23 12:43:07 +0700

    init

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -11,7 +11,7 @@ export default {
           status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
           tech: 'VueJS 3, Tailwind',
           github: 'https://github.com/YouLikeADream/Portovolio_V2',
-          demo: 'https://ferdiansyah.vercel.app/'
+          demo: 'https://ferdiansyah.xyz'
         }
       ]
     };
