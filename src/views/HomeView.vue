c0564906b84cc15be489dbccff53a91125c11cf8
YouLikeADream <vinlyyeus@gmail.com>
2024-07-24 15:45:25 +0700

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,7 @@
         </svg>Download Resume</button> -->
     </div>
     <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://i.pinimg.com/originals/ff/5a/4a/ff5a4a40e791587f611a1bd871766567.jpg">
+        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://i.pinimg.com/originals/83/06/4e/83064eb856f1a32778153a1b54645355.jpg">
     </div>
   </main>
 </template>
