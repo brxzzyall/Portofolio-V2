48e013c9d697be771f35a8255d279235fd94d053
brxzzyall <vinlyyeus@gmail.com>
2024-11-12 20:11:09 +0700

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,7 @@
         </svg>Download Resume</button> -->
     </div>
     <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="280" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-100 pict" src="img.itsme.jpg">
+        class="w-10/12 md:h-auto rounded-full border-4 border-amber-100 pict" src="https://vault.pictures/media/images/9f/c0/93/9fc093a5574844b096b429da80cee00c.jpg">
     </div>
   </main>
 </template>
