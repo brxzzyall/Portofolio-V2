b04372a4425af68243a2e8a84324a8c9f46a753c
brxzzyall <vinlyyeus@gmail.com>
2024-12-17 11:56:58 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,9 @@
         <div class="flex md:order-2 fadein-bot">
           <a href="https://github.com/github.com/brxzzyall"><img class="w-9 rounded-full" src="https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png" alt="github"></a>
         </div>
+        <div class="flex md:order-2 fadein-bot">
+          <a href="https://github.com/github.com/brxzzyall"><img class="w-9 rounded-full" src="https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png" alt="github"></a>
+        </div>
         <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
             <li>
